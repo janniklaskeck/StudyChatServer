31d0d5f56a5bae06ff5b07052d1531118c036abe
Jan-Niklas Keck <janniklaskeck@online.de>
2017-05-07 01:31:41 +0200

    asd

--- src/main/java/stud/mi/Application.java
+++ src/main/java/stud/mi/Application.java
@@ -1,6 +1,5 @@
 package stud.mi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import stud.mi.server.ChatServer;
@@ -12,7 +11,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Resource.class);
+
         String port = System.getenv("PORT");
         final ChatServer chatServer = new ChatServer(Integer.parseInt(port));
         chatServer.start();
