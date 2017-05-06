32f95ab8cc3144e2debfaca8ea8e6b3d045cfacf
Jan-Niklas Keck <janniklaskeck@online.de>
2017-05-07 01:28:00 +0200

    asd

--- src/main/java/stud/mi/Application.java
+++ src/main/java/stud/mi/Application.java
@@ -1,13 +1,18 @@
 package stud.mi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import stud.mi.server.ChatServer;
 
+@SpringBootApplication
 public class Application {
 
     private Application() {
     }
 
     public static void main(String[] args) {
+        SpringApplication.run(Resource.class);
         String port = System.getenv("PORT");
         final ChatServer chatServer = new ChatServer(Integer.parseInt(port));
         chatServer.start();
---
+++ src/main/java/stud/mi/Resource.java
@@ -0,0 +1,10 @@
+package stud.mi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Resource {
+
+}
