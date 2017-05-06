263bcfcecd9b233e397cad7111f6f040d9a12068
Jan-Niklas Keck <janniklaskeck@online.de>
2017-05-07 01:35:40 +0200

    qwe

--- src/main/java/stud/mi/Application.java
+++ src/main/java/stud/mi/Application.java
@@ -1,10 +1,7 @@
 package stud.mi;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import stud.mi.server.ChatServer;
 
-@SpringBootApplication
 public class Application {
 
     private Application() {
@@ -13,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
 
         String port = System.getenv("PORT");
+        System.out.println(port);
         final ChatServer chatServer = new ChatServer(Integer.parseInt(port));
         chatServer.start();
     }
