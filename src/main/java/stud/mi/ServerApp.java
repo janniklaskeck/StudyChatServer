d6b3c671b96cb59aedda2eab005c73db2e308db1
Jan-Niklas Keck <janniklaskeck@online.de>
2017-05-07 22:53:29 +0200

    update

---
+++ src/main/java/stud/mi/ServerApp.java
@@ -0,0 +1,27 @@
+package stud.mi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import stud.mi.server.ChatServer;
+
+public class ServerApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerApp.class);
+
+    private ChatServer chatServer;
+
+    private ServerApp() {
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "80";
+        }
+        chatServer = new ChatServer(Integer.parseInt(port));
+        chatServer.start();
+        LOGGER.debug("Server started on port: {}", port);
+    }
+
+    public static void main(String[] args) {
+        new ServerApp();
+    }
+}
