645c39e8c4196536edbe30ec528ad1509edc7e5d
Jan-Niklas Keck <janniklaskeck@online.de>
2017-08-02 20:43:12 +0200

    add channel history logic

--- src/main/java/stud/mi/message/Message.java
+++ src/main/java/stud/mi/message/Message.java
@@ -3,55 +3,83 @@ package stud.mi.message;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-public class Message {
+public class Message
+{
 
-	private static final JsonParser JSON_PARSER = new JsonParser();
+    private static final JsonParser JSON_PARSER = new JsonParser();
+    private static final String REV = "_rev";
+    private static final String ID = "_id";
 
-	private JsonObject jsonData;
+    private JsonObject jsonData;
 
-	public Message(final String json) {
-		jsonData = JSON_PARSER.parse(json).getAsJsonObject();
-	}
+    public Message(final String json)
+    {
+        this.jsonData = JSON_PARSER.parse(json).getAsJsonObject();
+        this.addMissingFields();
+    }
 
-	public Message(final JsonObject json) {
-		this.jsonData = json;
-	}
+    public Message(final JsonObject json)
+    {
+        this.jsonData = json;
+        this.addMissingFields();
+    }
 
-	public int getVersion() {
-		return jsonData.get("version").getAsInt();
-	}
+    private void addMissingFields()
+    {
+        if (this.jsonData.get(REV) == null)
+        {
+            this.jsonData.addProperty(REV, "");
+        }
+        if (this.jsonData.get(ID) == null)
+        {
+            this.jsonData.addProperty(ID, "0");
+        }
+    }
 
-	public String getType() {
-		return jsonData.get("type").getAsString();
-	}
+    public int getVersion()
+    {
+        return this.jsonData.get("version").getAsInt();
+    }
 
-	public Long getUserID() {
-		return getContent().get("userID").getAsLong();
-	}
+    public String getType()
+    {
+        return this.jsonData.get("type").getAsString();
+    }
 
-	public String getUserName() {
-		return getContent().get("userName").getAsString();
-	}
+    public Long getUserID()
+    {
+        return this.getContent().get("userID").getAsLong();
+    }
 
-	public String getMessage() {
-		return getContent().get("message").getAsString();
-	}
+    public String getUserName()
+    {
+        return this.getContent().get("userName").getAsString();
+    }
 
-	public String getChannelName() {
-		return getContent().get("channelName").getAsString();
-	}
+    public String getMessage()
+    {
+        return this.getContent().get("message").getAsString();
+    }
 
-	public JsonObject getContent() {
-		return jsonData.get("content").getAsJsonObject();
-	}
+    public String getChannelName()
+    {
+        return this.getContent().get("channelName").getAsString();
+    }
 
-	@Override
-	public String toString() {
-		return "";
-	}
+    public JsonObject getContent()
+    {
+        return this.jsonData.get("content").getAsJsonObject();
+    }
 
-	public String toJson() {
-		return jsonData.toString();
-	}
+    @Override
+    public String toString()
+    {
+        return "";
+    }
+
+    public String toJson()
+    {
+        return this.jsonData.toString();
+    }
 
 }
--- src/main/java/stud/mi/server/channel/Channel.java
+++ src/main/java/stud/mi/server/channel/Channel.java
@@ -2,11 +2,22 @@ package stud.mi.server.channel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import stud.mi.message.Message;
 import stud.mi.message.MessageType;
@@ -19,6 +30,11 @@ public class Channel
     private static final Logger LOGGER = LoggerFactory.getLogger(Channel.class);
     private static final int MAX_USERS = 512;
     private static final int DEFAULT_MAX_USERS = 64;
+    private static final String HISTORY_URL_TEMPLATE = "https://studychatbackend.mybluemix.net/history/channel/%s";
+    private static final String HISTORY_URL_TEMPLATE_LOCAL = "http://127.0.0.1:8089/history/channel/%s";
+
+    private static final SslContextFactory sslContextFactory = new SslContextFactory();
+    private static final HttpClient client = new HttpClient(sslContextFactory);
 
     private final List<RemoteUser> userList = new ArrayList<>();
     private String name;
@@ -39,6 +55,16 @@ public class Channel
         {
             this.maxUsers = maxUsers;
         }
+
+        try
+        {
+            client.start();
+        }
+        catch (final Exception e)
+        {
+            LOGGER.error("Could not start HTTP Client.", e);
+        }
+
         LOGGER.debug("Channel created with name {} and {} maximum Users", this.name, this.maxUsers);
     }
 
@@ -66,6 +92,7 @@ public class Channel
             break;
         case MessageType.CHANNEL_MESSAGE:
             msg = MessageBuilder.buildMessagePropagateAnswer(message.getMessage(), sender.getName());
+            this.addMessageToHistory(msg);
             break;
         default:
             msg = new Message("{}");
@@ -81,10 +108,12 @@ public class Channel
 
     public boolean userJoin(final RemoteUser user)
     {
+        LOGGER.debug("User {} wants to join Channel {}", user.getName(), this.getName());
         if (this.userList.size() < this.maxUsers)
         {
             this.userList.add(user);
             this.sendMessageToChannel(user, MessageType.CHANNEL_USER_CHANGE);
+            this.sendHistoryToUser(user);
             return true;
         }
         return false;
@@ -92,9 +121,75 @@ public class Channel
 
     public boolean userExit(final RemoteUser user)
     {
+        LOGGER.debug("User {} wants to exit Channel {}", user.getName(), this.getName());
         final boolean success = this.userList.remove(user);
         this.sendMessageToChannel(user, MessageType.CHANNEL_USER_CHANGE);
         return success;
+    }
+
+    private void sendHistoryToUser(final RemoteUser user)
+    {
+        LOGGER.info("Retreive and send Channel {} Chat History to user {}", this.getName(), user.getName());
+        final JsonArray chatHistory = new JsonArray();
+        final List<Message> currentChannelChatHistory = this.getCurrentChannelChatHistory();
+        for (final Message msg : currentChannelChatHistory)
+        {
+            final JsonElement jo = new JsonParser().parse(msg.toJson());
+            chatHistory.add(jo);
+        }
+        final Message msg = MessageBuilder.buildChannelHistoryMessage(chatHistory, this);
+        user.getConnection().send(msg.toJson());
+        LOGGER.debug("Send Message '{}' to User '{}'", msg.toJson(), user.getName());
+    }
+
+    private List<Message> getCurrentChannelChatHistory()
+    {
+        LOGGER.debug("Retreiving Chat History for Channel {}", this.getName());
+        final List<Message> messageHistory = new ArrayList<>();
+        if (client.isStarted())
+        {
+            String response = "";
+            final Request newRequest = client.newRequest(String.format(this.getBackendUrlTemplate(), this.getName().toLowerCase()));
+            try
+            {
+                response = newRequest.send().getContentAsString();
+                LOGGER.debug("{}", response);
+                final JsonObject messageListObject = new JsonParser().parse(response).getAsJsonObject();
+                if (messageListObject.get("messages") != null)
+                {
+                    final JsonArray messagesArray = messageListObject.get("messages").getAsJsonArray();
+                    for (final JsonElement element : messagesArray)
+                    {
+                        messageHistory.add(new Message(element.getAsJsonObject()));
+                    }
+                }
+            }
+            catch (InterruptedException | TimeoutException | ExecutionException e)
+            {
+                LOGGER.error("HTTP Get not executed successfully!", e);
+            }
+
+        }
+        return messageHistory;
+    }
+
+    private boolean addMessageToHistory(final Message message)
+    {
+        if (client.isStarted())
+        {
+            final Request newRequest = client.POST(String.format(this.getBackendUrlTemplate(), this.getName().toLowerCase()));
+            newRequest.content(new StringContentProvider(message.toJson()), "application/json");
+            try
+            {
+                final ContentResponse response = newRequest.send();
+                return response.getStatus() == HttpStatus.OK_200;
+            }
+            catch (InterruptedException | TimeoutException | ExecutionException e)
+            {
+                LOGGER.error("HTTP Get not executed successfully!", e);
+            }
+        }
+        return false;
     }
 
     public List<RemoteUser> getUserList()
@@ -110,5 +205,14 @@ public class Channel
     public String getName()
     {
         return this.name;
+    }
+
+    public String getBackendUrlTemplate()
+    {
+        if (System.getenv("PORT") == null)
+        {
+            return HISTORY_URL_TEMPLATE_LOCAL;
+        }
+        return HISTORY_URL_TEMPLATE;
     }
 }
